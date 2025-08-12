e3fa7b130ac3ae10f47e8bd60a4289da38f29f9a
Mikel Garrido <mikelgarridolinares@gmail.com>
2025-08-12 21:25:23 +0200

    iscelanius

--- src/steps/tokenizer/utils/is_word.c
+++ src/steps/tokenizer/utils/is_word.c
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 19:43:39 by migarrid          #+#    #+#             */
-/*   Updated: 2025/08/12 18:48:18 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/08/12 21:24:34 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	is_word(t_shell *data, t_token *tokens, const char *str, int *i)
 		word = ft_substr(str, start, len);
 		if (!word)
 			exit_error(data, ERR_MALLOC, EXIT_FAILURE);
-		printf("Token creado: '%s'\n", word); // DEBUG
+		printf("Token palabra creado: '%s'\n", word); // DEBUG
 		add_token(tokens, word, WORD);
 		// is_word_or_cmd(data, &data->prompt, tokens, word);
 	}
