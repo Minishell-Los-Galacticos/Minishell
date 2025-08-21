646de996552ec9db8ebbb1d26bc57887232af5e7
LordMikkel <125906789+LordMikkel@users.noreply.github.com>
2025-08-21 22:27:57 +0200

    Norminette

--- src/main/print_session_start.c
+++ src/main/print_session_start.c
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 02:13:28 by davdiaz-          #+#    #+#             */
-/*   Updated: 2025/08/21 21:57:51 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/08/21 22:25:16 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,23 @@
 	día, mes y año.Luego llama a la función print_time_of_day para mostrar un
 	mensaje personalizado según el momento del día en que se inicia la sesión.
 */
+void	print_minishell_title(void)
+{
+	printf(TITLE_COLOR "\n\n");
+	printf(T1 "\n");
+	printf(T2 "\n");
+	printf(T3 "\n");
+	printf(T4 "\n");
+	printf(T5 "\n");
+	printf("\n\n" RESET_COLOR);
+}
 
 void	print_session_start(time_t start)
 {
 	struct tm	*local;
 
 	local = localtime(&start);
-	ft_printf_fd(STDOUT, MSG_TITLE_4);
+	print_minishell_title();
 	print_time_of_day(start);
 	printf(MSG_TIME_START,
 		local->tm_hour, local->tm_min, local->tm_sec,
