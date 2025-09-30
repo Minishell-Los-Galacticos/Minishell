d65c0381cd9cdddba26d0e14ca8d7eb8635afeff
David Diaz Bastidas <diaz.dbd@gmail.com>
2025-10-01 00:11:31 +0200

    Algunos upgrades

--- src/executor/utils/which_builtin.c
+++ src/executor/utils/which_builtin.c
@@ -6,34 +6,63 @@
 /*   By: davdiaz- <davdiaz-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 21:58:35 by migarrid          #+#    #+#             */
-/*   Updated: 2025/09/28 02:36:14 by davdiaz-         ###   ########.fr       */
+/*   Updated: 2025/09/29 20:35:39 by davdiaz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
 
-void	which_builtin(t_shell *data, t_token *tokens, t_token *token)
+static void asignations(t_shell *data, t_token *token)
 {
 	if (token->type == ASIGNATION)
 		data->last_exit_code = asignation(data, token, LOCAL);
-	if (token->type == PLUS_ASIGNATION)
+	else if (token->type == PLUS_ASIGNATION)
 		data->last_exit_code = asignation(data, token, PLUS_ASIGNATION);
-	if (token->type == TEMP_ASIGNATION)
+	else if (token->type == TEMP_ASIGNATION)
 		data->last_exit_code = asignation(data, token, TEMP_ASIGNATION);
-	if (ft_strcmp(token->value, BUILTIN_ECHO) == 0)
-		data->last_exit_code = my_echo(&data->prompt, token);
-	else if (ft_strcmp(token->value, BUILTIN_PWD) == 0)
-		data->last_exit_code = my_pwd(data);
-	else if (ft_strcmp(token->value, BUILTIN_ECHO) == 0)
-		data->last_exit_code = my_echo(&data->prompt, token);
-	else if (ft_strcmp(token->value, BUILTIN_EXPORT) == 0)
+}
+
+static void	env_commands(t_shell *data, t_token *tokens, t_token *token)
+{
+	int flag_error;
+
+	flag_error = 0;
+	if (ft_strmatch_cmp(token->value, BUILTIN_EXPORT, &flag_error) == 0)
 		data->last_exit_code = my_export(data, tokens, token, &data->env);
-	else if (ft_strcmp(token->value, BUILTIN_UNSET) == 0)
+	else if (ft_strmatch_cmp(token->value, BUILTIN_UNSET, &flag_error) == 0)
 		data->last_exit_code = my_unset(data, &data->env, tokens);
-	else if (ft_strcmp(token->value, BUILTIN_ENV) == 0)
+	else if (ft_strmatch_cmp(token->value, BUILTIN_ENV, &flag_error) == 0)
 		data->last_exit_code = my_env(data->env.vars);
-	else if (ft_strcmp(token->value, BUILTIN_CD) == 0)
+	if (flag_error == ERROR)
+		exit_error(data, ERR_MALLOC, EXIT_FAILURE);
+}
+
+
+static void	basic_builtins(t_shell *data, t_token *tokens, t_token *token)
+{
+	int flag_error;
+
+	flag_error = 0;
+	if (ft_strmatch_cmp(token->value, BUILTIN_ECHO, &flag_error) == 0)
+	data->last_exit_code = my_echo(&data->prompt, token);
+	else if (ft_strmatch_cmp(token->value, BUILTIN_PWD, &flag_error) == 0)
+		data->last_exit_code = my_pwd(data);
+	else if (ft_strmatch_cmp(token->value, BUILTIN_CD, &flag_error) == 0)
 		data->last_exit_code = my_cd(data, tokens, token);
-	else if (ft_strcmp(token->value, BUILTIN_EXIT) == 0)
+	else if (ft_strmatch_cmp(token->value, BUILTIN_EXIT, &flag_error) == 0)
 		my_exit(data, &data->prompt, token);
+	if (flag_error == ERROR)
+		exit_error(data, ERR_MALLOC, EXIT_FAILURE);
+}
+
+void	which_builtin(t_shell *data, t_token *tokens, t_token *token)
+{
+	int flag_error;
+
+	flag_error = 0;
+	asignations(data, token);
+	env_commands(data, tokens, token);
+	basic_builtins(data, tokens, token);
+	if (flag_error == ERROR)
+		exit_error(data, ERR_MALLOC, EXIT_FAILURE);
 }
