ca93fd13f96576d79314c90f0e8a75abb6e67ec2
migarrid <migarrid@student.42barcelona.com>
2025-10-07 16:44:13 +0200

    builtins cambiados a args

--- lib/libft_plus/libft/ft_strmatch_cmp.c
+++ lib/libft_plus/libft/ft_strmatch_cmp.c
@@ -6,7 +6,7 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 19:54:17 by davdiaz-          #+#    #+#             */
-/*   Updated: 2025/10/05 22:44:22 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/10/07 16:39:07 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,50 +27,18 @@ static void	ft_compare_and_transform(char **temp, const char *s2)
 	}
 }
 
-int	ft_strmatch_cmp(char *s1, const char *s2, int *error)
+int	ft_strmatch_cmp(char *s1, const char *s2)
 {
 	char	*temp;
 	int		result;
 
+	if (!s1 || !s2)
+		return (0);
 	temp = ft_strdup(s1);
 	if (!temp)
-	{
-		*error = -1;
 		return (-1);
-	}
 	ft_compare_and_transform(&temp, s2);
 	result = ft_strcmp(temp, s2);
 	free (temp);
 	return (result);
 }
-
-// static void	ft_compare_and_transform(char **temp, const char *s2)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while ((*temp)[i] != '\0' && s2[i] != '\0')
-// 	{
-// 		if (ft_tolower((*temp)[i]) == s2[i])
-// 			(*temp)[i] = ft_tolower((*temp)[i]);
-// 		else if (ft_toupper((*temp)[i]) == s2[i])
-// 			(*temp)[i] = ft_toupper((*temp)[i]);
-// 		i++;
-// 	}
-// }
-
-// int	ft_strmatch_cmp(char *s1, const char *s2)
-// {
-// 	char	*temp;
-// 	int		result;
-
-// 	if (!s1 || !s2)
-// 		return (0);
-// 	temp = ft_strdup(s1);
-// 	if (!temp)
-// 		return (-1);
-// 	ft_compare_and_transform(&temp, s2);
-// 	result = ft_strcmp(temp, s2);
-// 	free (temp);
-// 	return (result);
-// }
--- src/tokenizer/utils/trasnform_tokens/transform_command_builtin_lowercase.c
+++ src/tokenizer/utils/trasnform_tokens/transform_command_builtin_lowercase.c
@@ -6,40 +6,33 @@
 /*   By: migarrid <migarrid@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 19:40:49 by migarrid          #+#    #+#             */
-/*   Updated: 2025/10/06 20:00:11 by migarrid         ###   ########.fr       */
+/*   Updated: 2025/10/07 16:08:19 by migarrid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../../inc/minishell.h"
 
-void	normalize_token_to_lower(t_shell *data, t_token *token, char *og_value)
+void	normalize_token_to_lower(t_token *token)
 {
 	int		i;
-	char	*lc_value;
 
 	i = 0;
-	lc_value = ft_calloc(ft_strlen(og_value) + 1, sizeof(char));
-	if (!lc_value)
-		return ((void)exit_error(data, ERR_MALLOC, EXIT_FAIL));
-	while(og_value[i] != '\0')
+	while(token->value[i])
 	{
-		lc_value[i] = ft_tolower(og_value[i]);
+		token->value[i] = ft_tolower(token->value[i]);
 		i++;
 	}
-	lc_value[i] = '\0';
-	free(og_value);
-	token->value = lc_value;
 }
 
-void	transform_command_built_lowercase(t_shell *d, t_prompt *p, t_token *t)
+void	transform_command_built_lowercase(t_prompt *prompt, t_token *tokens)
 {
 	int	i;
 
 	i = 0;
-	while  (i < p->n_tokens)
+	while  (i < prompt->n_tokens)
 	{
-		if (is_cmd_builtin_type(t[i].type))
-			normalize_token_to_lower(d, &t[i], t->value);
+		if (is_cmd_builtin_type(tokens[i].type))
+			normalize_token_to_lower(&tokens[i]);
 		i++;
 	}
 }
